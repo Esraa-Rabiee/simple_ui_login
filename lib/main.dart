dce6c7562536069aa2dce4a3e4ebac0d81a7cf25
Esraa Rabie <52860344+Esraa-Rabie-Elewa@users.noreply.github.com>
2019-07-13 17:35:08 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Center(
+            child: Text("mi_card"),
+          ),
+        ),
+        backgroundColor: Colors.white30,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("images/hey.jpeg"),
+              ),
+              Text(
+                "Esraa rabie",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: "ShadowsIntoLight"),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                    fontSize: 20, color: Colors.white, letterSpacing: 2.51),
+              ),
+              SizedBox(width:50,height: 10,child:Divider(color: Colors.white,),),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 50, vertical: 50),
+                child: ListTile(
+                  leading: Icon(Icons.phone),
+                  title: Text("+01123456789"),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 50),
+                child: ListTile(
+                    leading: Icon(Icons.mail, color: Colors.orange),
+                    title: Text("esraarabie@yahoo.com")),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
